003ee120e289d17018b8def6c59a85f631c93afe
smilingprogrammer <abdulsoburoyewale@gmail.com>
2021-11-13 06:57:29 +0100

    Tip UI

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.tipcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tipcalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    fun calculateTip() {
+        val stringInField = binding.costOfService.text.toString()
+        val cost = stringInField.toDouble()
+
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+
+        val percentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        val tip = percentage * cost
     }
 }
